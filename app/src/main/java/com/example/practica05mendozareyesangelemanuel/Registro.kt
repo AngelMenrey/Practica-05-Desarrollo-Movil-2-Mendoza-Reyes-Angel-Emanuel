d2ec22755ed5b61870180b9399e733a8853d7944
Angel Reyes <a22110083@ceti.mx>
2024-09-25 19:57:14 -0600

    Práctica 05 - Gestión de cámara - Mendoza Reyes Angel Emanuel

---
+++ app/src/main/java/com/example/practica05mendozareyesangelemanuel/Registro.kt
@@ -0,0 +1,8 @@
+package com.example.practica05mendozareyesangelemanuel
+
+data class Registro(
+    val codigo: String,
+    val descripcion: String,
+    val precio: Double,
+    val cantidad: Int
+)
